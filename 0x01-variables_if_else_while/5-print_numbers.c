381e204eef4a816250ec3ea2a49c839c295386c2
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-17 17:54:05 +0000

    Task: Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all single digit numbers in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+
+	{
+		printf("%i\n", n);
+	}
+	return (0);
+}
