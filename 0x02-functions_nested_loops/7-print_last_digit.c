e79ed19c217c326b72ba805609c18dc06c3b2012
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-21 22:02:41 +0000

    Task: There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important mandator

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,15 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
-
-	_putchar('0' + (n % 10));
-	return (n % 10);
+	if (n > 0 || n == 0)
+	{
+		_putchar (n % 10 + '0');
+		return (n % 10);
+	}
+	else
+	{
+		n = n * -1;
+		_putchar (n % 10 + '0');
+		return (n % 10);
+	}
 }
