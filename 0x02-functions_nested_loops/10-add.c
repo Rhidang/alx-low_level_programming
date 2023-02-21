efd0c180a85f644cdd2ab05eedbf6ba1ed69b8f5
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-21 23:28:01 +0000

    Task: a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @x: integer 1
+ * @y: integer 2
+ * Description: adds two numbers
+ * Return: value of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
