cb765dfb4d3ada175102561b6318a4c4d32e19f4
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-22 12:16:33 +0000

    Task: 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,20 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
-		a++;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
-		b++;
 	}
 	else
 	{
 		largest = c;
-		c++;
 	}
+
 	return (largest);
 }
