b3a0e5e13070a6f98f19ed501395bb26c8d87e6e
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-23 22:38:55 +0000

    Task: Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: the FizzBuzz challenge
+ * Return: zero
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", a);
+		if (a < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
