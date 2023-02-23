3e653ffaba1e65ebf01cb4d5af64565074aff214
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-23 19:11:42 +0000

    Task: The shortest distance between two points is a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: number of times to print line
+ * Description: Print a straight line
+ * Return zero
+ */
+void print_line(int n)
+{
+	int n;
+
+	if (n > 0; n <= 10)
+	{
+		_putchar(_);
+		n++;
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
