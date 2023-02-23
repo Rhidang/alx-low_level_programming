2a91cd492b1462607679cc3a768497dacb84f683
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-23 10:51:50 +0000

    Task: Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: first integer
+ * @b: second integer
+ * Description: Multiplies two integers
+ * Return: value of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
