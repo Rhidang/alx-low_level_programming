d8692fb830f7d0b3027b74fc9b8cfe36298ac054
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-23 17:56:49 +0000

    Task: I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,13 @@ void print_most_numbers(void)
 {
 	char i;
 
-	for (i = 0; i < 10; i++)
+	while (i < '10')
 	{
-		if (i != 2 && i != 4)
+		if (i != '2' && i != '4')
+		{
 			_putchar(i);
+		}
+		i++;
 	}
-	return (0);
+	_putchar('\n')
 }
