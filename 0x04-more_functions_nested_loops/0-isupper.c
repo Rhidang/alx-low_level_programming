5d11378d443cd290b9d0e1f47f103f3796abb8a4
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-23 10:33:30 +0000

    Task: isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - function for the task
+ * Description: Checks if c is upper or lowercase
+ * @c: character to check
+ * Return: one if uppercase, zero otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
