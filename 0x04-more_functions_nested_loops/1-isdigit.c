1768091c4a386a568a6e84ca0487491e6793dea6
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-23 10:44:22 +0000

    Task: isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point.
+ * Description: Checks if c is a digit
+ * @c: character to check
+ * Return: one if successful, zero otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c < 10)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
