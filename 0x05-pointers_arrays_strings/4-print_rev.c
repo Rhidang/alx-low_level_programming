c0da6007b03d58b58f839a03810489e74680b87b
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-27 14:21:46 +0000

    Task: I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - function to use
+ * @s: String to reverse
+ * Description: reverse a string
+ * Return: Zero
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+		{
+		}
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
