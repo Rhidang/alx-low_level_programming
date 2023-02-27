6ff20017d9ca9381b54b402047c99637d1b49b1f
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-27 12:53:31 +0000

    Task: This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - Function to use
+ * @s: String to check
+ * Description: To check the length of a string
+ * Return: Always zero
+ */
+int _strlen(char *s)
+{
+	char s;
+	int len;
+
+	len = strlen(s);
+}
