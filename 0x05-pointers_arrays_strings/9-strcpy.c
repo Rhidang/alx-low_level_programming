9fade19f32530c4d32ebf0155cc348a354302efc
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-02-27 17:02:35 +0000

    Task: strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,34 @@
 #include "main.h"
 /**
+ * _strlen - Function to use
+ * @s: String to check
+ * Description: To check the length of a string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
  * *_strcpy - Function to use
- * @dest: pointer
- * @src: pointer
- * Description: TO copy one string to another
+ * @dest: Pointer
+ * @src: Pointer
+ * Description: Copies string.
  * Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
-	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+		dest[i] = src[i];
 	return (dest);
 }
