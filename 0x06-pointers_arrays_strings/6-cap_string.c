257cc553029ec6fcb56f1cb0e345ebcb0dc765e3
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-01 13:32:49 +0000

    Task: Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *cap_string: Function to use
+ * @s: String to change
+ * Description: Function to capitalize all words of a string
+ * Return: String
+ */
+char *cap_string(char *s)
+{
+	int a = 0;
+
+	if (s[a] >= 'a'; && s[a] <= 'z')
+		s[a] = s[a] - 'a' + 'A';
+	a++;
+
+	while (s[a] != '\0')
+	{
+		if ((s[a] >= 'a' && s[a] <= 'z')
+				&& (s[a - 1] == ',' || s[a - 1] == ';' || s[a - 1] == '.' ||
+					s[a - 1] == '!' || s[a - 1] == '?' || s[a - 1] == '"' ||
+					s[a - 1] == '(' || s[a - 1] == ')' || s[a - 1] == '{' ||
+					s[a - 1] == '}' || s[a - 1] == ' ' || s[a - 1] == '\t' ||
+					s[a - 1] == '\n'))
+			s[a] = s [a] - 'a' + 'A'
+				a++
+	}
+	return (s);
+}
+
