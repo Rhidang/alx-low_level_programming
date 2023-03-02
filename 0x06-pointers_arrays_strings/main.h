530d5e32b8de99b52a8c3d38188940e8fefd1025
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-02 11:15:55 +0000

    Task:  It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ char *cap_string(char *s);
 char *leet(char *l);
 char *rot13(char *r);
 void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
