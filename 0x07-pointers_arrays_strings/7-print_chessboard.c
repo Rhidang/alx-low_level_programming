c2d48057cf49cab977f048f0334048ceb249aa66
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-06 18:04:22 +0000

    Task: Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - Function to use
+ * @a: Array
+ * Description: Function that prints a chessboard
+ * Return: Zero
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for column = 0; column < 8; column++)
+			_putchar(a[row][column]);
+		_putchar('\n');
+	}
+}
