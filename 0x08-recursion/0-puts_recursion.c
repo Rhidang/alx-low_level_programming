5a29c9caf479918e903d0878084c69c0d60d3e27
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-08 11:36:10 +0000

    Task: She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - Function to use
+ * @s: String to print
+ * Description: Function to print a string
+ * Return: String
+ */
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+		s++;
+	return (s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+ALX SWE tasks on recursion
