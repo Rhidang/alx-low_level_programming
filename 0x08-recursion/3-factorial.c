680338cdc9b8a6bd19fcf1550db9b1d97ddb166e
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-08 12:55:38 +0000

    Task: You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main"
+/**
+ * factorial - Function to use
+ * @n: Number to find factorial of
+ * Description: Function to find factorial of a number
+ * Return: -1 on error
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
