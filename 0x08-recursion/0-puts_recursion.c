538cfab155b4206ee34efc24c92bbfc3e43e307a
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-08 12:37:59 +0000

    Corrections to task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s != '\0')
 	{
-		_puts_recursion(s);
-		s++;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
