d1c20e6cbf657d9e003045240cb1d8a882f4a475
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-08 14:14:52 +0000

    Task: Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * a_prime - Function to use
+ * @n: Number to use
+ * @x: Divisor
+ * Description: Function  to find the prime of a number
+ * Return: prime
+ */
+int a_prime(int n, int x)
+{
+	if (n == x)
+	{
+		return (1);
+	}
+	if (n % y == 0)
+	{
+		return (0);
+	}
+	return (a_prime(n, y + 1));
+}
+/**
+ * is_prime_number - Function to use
+ * @n: Number to use
+ * Description: Function to check prime
+ * Return: prime
+ */
+int is_prime_number(int n)
+{
+	int x = 3;
+
+	if (n % 2 == 0 || n < 2)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	return(a_prime(n, x));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int root(int n, int y);
+int a_prime(int n, int x);
+int is_prime_number(int n);
 
 
 
