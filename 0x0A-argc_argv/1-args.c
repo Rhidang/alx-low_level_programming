063927ab9306c9af84ecf0f082e92f247d71e587
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-10 18:27:46 +0000

    Task: Silence is argument carried out by other means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,9 +2,10 @@
 /**
  * main - Function to use
  * @argc: Argument counter
+ * @argv: Argument vector
  * Return: Zero
  */
-int main(int argc)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
