701f39ac7099ec5cbd6bac5299f64d871f025f13
Mordecai Maisaini <maisainimordecai@gmail.com>
2023-03-13 10:21:39 +0000

    Task:  He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, j, k;
-	char an_array;
+	char *an_array;
 
 	k = 0;
 	an_array = malloc(sizeof(*s1 + *s2));
@@ -19,14 +19,14 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	i = 0;
-	while (s1[a] != '\0')
+	while (s1[i] != '\0')
 	{
 		an_array[k] = s1[i];
 		i++;
 		k++;
 	}
 	j = 0;
-	while (s2[b] != '\0')
+	while (s2[j] != '\0')
 	{
 		an_array[k] = s2[j];
 		j++;
